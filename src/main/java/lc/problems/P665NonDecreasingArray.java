2b5ff88dd9cb4b578e912eea5c987b1432a25f49
Zheng Li <zli@powercosts.com>
2018-05-23 12:13:41 -0500

    add P665
    Change-Id: I501031e72f6b05b76851c79d925ada4b8e934ba7
    Signed-off-by: Zheng Li <zli@powercosts.com>

---
+++ src/main/java/lc/problems/P665NonDecreasingArray.java
@@ -0,0 +1,43 @@
+package lc.problems;
+
+/*
+ * Given an array with n integers, your task is to check if it could become non-decreasing by
+ * modifying at most 1 element.
+ * 
+ * We define an array is non-decreasing if array[i] <= array[i + 1] holds for every i (1 <= i < n).
+ * 
+ * Example 1: Input: [4,2,3] Output: True Explanation: You could modify the first 4 to 1 to get a
+ * non-decreasing array. Example 2: Input: [4,2,1] Output: False Explanation: You can't get a
+ * non-decreasing array by modify at most one element. Note: The n belongs to [1, 10,000].
+ */
+public class P665NonDecreasingArray {
+  public boolean checkPossibility(int[] nums) {
+    if (nums.length < 3) {
+      return true;
+    }
+    int N = nums.length;
+    int count = 0;
+    for (int i = 0; i < N - 1; i++) {
+      if (nums[i] > nums[i + 1]) {
+        if (i == 0 || i == N - 1 || nums[i - 1] <= nums[i + 1]) {
+          if (i > 0)
+            nums[i] = nums[i - 1];
+          if (++count > 1)
+            return false;
+        } else
+          return false;
+      }
+    }
+    return true;
+  }
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    P665NonDecreasingArray p = new P665NonDecreasingArray();
+    System.out.println(p.checkPossibility(new int[] {-1, 4, 2, 3}));
+    System.out.println(p.checkPossibility(new int[] {1, 1, 3, 5, 2, 4, 9}));
+    System.out.println(p.checkPossibility(new int[] {4, 2, 3}));
+    System.out.println(p.checkPossibility(new int[] {2, 3, 3, 2, 4}));
+  }
+
+}
