1b521d644e686e9c3d5d207c55757c5395b9bd8a
Zheng Li <zli@powercosts.com>
2018-05-23 12:13:41 -0500

    add P665
    Change-Id: I501031e72f6b05b76851c79d925ada4b8e934ba7
    Signed-off-by: Zheng Li <zli@powercosts.com>

--- src/main/java/lc/problems/P665NonDecreasingArray.java
+++ src/main/java/lc/problems/P665NonDecreasingArray.java
@@ -11,33 +11,54 @@ package lc.problems;
  * non-decreasing array by modify at most one element. Note: The n belongs to [1, 10,000].
  */
 public class P665NonDecreasingArray {
+  
   public boolean checkPossibility(int[] nums) {
-    if (nums.length < 3) {
-      return true;
-    }
-    int N = nums.length;
-    int count = 0;
-    for (int i = 0; i < N - 1; i++) {
-      if (nums[i] > nums[i + 1]) {
-        if (i == 0 || i == N - 1 || nums[i - 1] <= nums[i + 1]) {
-          if (i > 0)
-            nums[i] = nums[i - 1];
-          if (++count > 1)
+    if (nums.length > 2) {
+      int N = nums.length;
+      int count = 0;
+
+      for (int i = 0; i < N - 1; i++) {
+        if (nums[i] > nums[i + 1]) {
+          if (i == 0 || i == N - 1) {
+            if (++count > 1) {
+              return false;
+            }
+          } else if (N > 3 && i < N - 2 && isOutOfRange(nums[i], nums[i - 1], nums[i + 2])
+              && isOutOfRange(nums[i + 1], nums[i - 1], nums[i + 2])) {
             return false;
-        } else
-          return false;
+          } else {
+            if (++count > 1) {
+              return false;
+            }
+          }
+        }
       }
     }
     return true;
   }
 
+  private boolean isOutOfRange(int number, int a, int b) {
+    int min, max;
+    if (a < b) {
+      min = a;
+      max = b;
+    } else {
+      min = b;
+      max = a;
+    }
+    return number < min || number > max;
+  }
+
   public static void main(String[] args) {
     // TODO Auto-generated method stub
     P665NonDecreasingArray p = new P665NonDecreasingArray();
-    System.out.println(p.checkPossibility(new int[] {-1, 4, 2, 3}));
-    System.out.println(p.checkPossibility(new int[] {1, 1, 3, 5, 2, 4, 9}));
-    System.out.println(p.checkPossibility(new int[] {4, 2, 3}));
-    System.out.println(p.checkPossibility(new int[] {2, 3, 3, 2, 4}));
+    assert p.checkPossibility(new int[] {-1, 4, 2, 3}) == true;
+    assert p.checkPossibility(new int[] {1, 1, 3, 5, 2, 4, 9}) == false;
+    assert p.checkPossibility(new int[] {4, 2, 3}) == true;
+    assert p.checkPossibility(new int[] {2, 3, 3, 2, 4}) == true;
+    assert p.checkPossibility(new int[] {4, 2, 1}) == false;
+    assert p.checkPossibility(new int[] {1, 2, 3, 5, 4}) == true;
+    assert p.checkPossibility(new int[] {3, 3, 2, 2}) == false;
   }
 
 }
