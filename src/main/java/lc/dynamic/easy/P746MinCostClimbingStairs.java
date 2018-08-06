934b47bc578999b486b71d5182f83c4d7b9734a9
Zheng Li <zli@powercosts.com>
2018-08-06 17:25:14 -0500

    Add P746
    Change-Id: Iaa85bf97bcb1cf6c305553522addd9b5566f0c46
    Signed-off-by: Zheng Li <zli@powercosts.com>

--- src/main/java/lc/dynamic/easy/P746MinCostClimbingStairs.java
+++ src/main/java/lc/dynamic/easy/P746MinCostClimbingStairs.java
@@ -1,5 +1,7 @@
 package lc.dynamic.easy;
 
+import org.junit.Assert;
+
 /**
  * @author zli
  *
@@ -12,12 +14,18 @@ package lc.dynamic.easy;
 public class P746MinCostClimbingStairs {
 
   public int minCostClimbingStairs(int[] cost) {
-    
+    if (cost == null || cost.length < 2)
+      return 0;
+
+    for (int i = 2; i < cost.length; i++) {
+      cost[i] += Math.min(cost[i - 1], cost[i - 2]);
+    }
+    return Math.min(cost[cost.length - 1], cost[cost.length - 2]);
   }
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
-
+    P746MinCostClimbingStairs p = new P746MinCostClimbingStairs();
+    Assert.assertEquals(15, p.minCostClimbingStairs(new int[] {10, 15, 20}));
+    Assert.assertEquals(6, p.minCostClimbingStairs(new int[] {1, 100, 1, 1, 1, 100, 1, 1, 100, 1}));
   }
-
 }
