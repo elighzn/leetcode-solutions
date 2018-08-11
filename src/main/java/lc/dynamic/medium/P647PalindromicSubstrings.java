92ccfda759fd27bb50f8f1cc17bcd59f5f3a9e34
Zheng Li <zli@powercosts.com>
2018-08-10 19:12:26 -0500

    Add P647
    Change-Id: I437a9a7aa7284eafe2833e58393384f20cf517bd

---
+++ src/main/java/lc/dynamic/medium/P647PalindromicSubstrings.java
@@ -0,0 +1,58 @@
+package lc.dynamic.medium;
+
+import org.junit.Assert;
+
+public class P647PalindromicSubstrings {
+  public int countSubstrings(String s) {
+    if (s == null || s.length() < 1)
+      return 0;
+
+    int total = s.length();
+    final int H1 = (s.length() - 2) / 2;
+
+    boolean[] b = new boolean[s.length()];
+
+    for (int i = 0; i < b.length - 1; i++)
+      if (s.charAt(i) == s.charAt(i + 1)) {
+        b[i] = true;
+        total++;
+      }
+
+    for (int l = 1; l <= H1; l++) {
+      for (int i = 1; i < s.length() - l - 1; i++) {
+        if (b[i])
+          if (s.charAt(i - l) == s.charAt(i + 1 + l))
+            total++;
+          else
+            b[i] = false;
+      }
+    }
+
+    for (int i = 0; i < b.length; i++)
+      b[i] = true;
+
+    final int H2 = (s.length() - 1) / 2;
+
+    for (int l = 1; l <= H2; l++) {
+      for (int i = 1; i < s.length() - l; i++) {
+        if (b[i]) {
+          if (s.charAt(i - l) == s.charAt(i + l))
+            total++;
+          else
+            b[i] = false;
+        }
+      }
+    }
+    return total;
+  }
+
+
+
+  public static void main(String[] args) {
+    P647PalindromicSubstrings p = new P647PalindromicSubstrings();
+    Assert.assertEquals(3, p.countSubstrings("aaaaa"));
+    Assert.assertEquals(3, p.countSubstrings("abc"));
+    Assert.assertEquals(6, p.countSubstrings("aaa"));
+
+  }
+}
