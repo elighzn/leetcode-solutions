285b4ca231d755513c5f25cffa28f2fe869f809f
Zheng Li <zli@powercosts.com>
2018-09-07 17:47:36 -0500

    Add P166
    Change-Id: Ibced30e70572149d6c3085d8ad3b30a24f6773b7
    Signed-off-by: Zheng Li <zli@powercosts.com>

--- src/main/java/lc/google/hard/P76MinimumWindowSubstring.java
+++ src/main/java/lc/google/hard/P76MinimumWindowSubstring.java
@@ -1,7 +1,7 @@
 package lc.google.hard;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -29,40 +29,109 @@ import org.junit.Assert;
  */
 public class P76MinimumWindowSubstring {
 
+	// public String minWindow(String s, String t) {
+	//
+	// if (s == null || t == null || s.length() < t.length())
+	// return "";
+	//
+	// Map<Character, LinkedList<Integer>> tMap = new HashMap<>();
+	//
+	// for (int i = 0; i < t.length(); i++)
+	// tMap.put(t.charAt(i), new LinkedList<>());
+	//
+	// for (int i = 0; i < s.length(); i++) {
+	// LinkedList<Integer> l = tMap.get(s.charAt(i));
+	// if (l != null)
+	// l.add(i);
+	// }
+	// int lo = -1, hi = s.length();
+	// for (LinkedList<Integer> list : tMap.values()) {
+	// if (list.isEmpty())
+	// return "";
+	//
+	// int first = list.getFirst(), last = list.getLast();
+	//
+	// if (first > lo)
+	// lo = first;
+	// if (last < hi)
+	// hi = last;
+	// }
+	//
+	// return s.substring(lo, hi);
+	// }
+
 	public String minWindow(String s, String t) {
 
-		if (s == null || t == null || s.length() < t.length())
+		if (s == null || t == null || s.isEmpty() || t.isEmpty() || s.length() < t.length())
 			return "";
 
-		Map<Character, LinkedList<Integer>> tMap = new HashMap<>();
+		int[] ans = new int[2];
+		Map<Character, Integer> tMap = new HashMap<>();
+		tMap.containsKey('a');
 
-		for (int i = 0; i < t.length(); i++)
-			tMap.put(t.charAt(i), new LinkedList<>());
+		for (char c : t.toCharArray())
+			tMap.put(c, 0);
+		int p1 = 0, p2 = 0;
+		boolean containAllChars = false;
 
-		for (int i = 0; i < s.length(); i++) {
-			LinkedList<Integer> l = tMap.get(s.charAt(i));
-			if (l != null)
-				l.add(i);
+		int minLength = Integer.MAX_VALUE;
+
+		while (p1 < s.length() || p2 < p1 - t.length() || (p2 == p1) && (p2 == 0)) {
+
+			if (containAllChars) {
+				char c = s.charAt(p2);
+				Integer ind = tMap.get(c);
+				if (ind != null) {
+					ind--;
+					if (ind == 0) {
+						containAllChars = false;
+						int length = p1 - p2;
+						if (length < minLength) {
+							ans[0] = p2;
+							ans[1] = p1;
+							minLength = length;
+						}
+						// if (p2 >= p1 - t.length())
+						// break;
+						// else
+						continue;
+					}
+				}
+				p2++;
+			} else {
+				char c = s.charAt(p1);
+				Integer ind = tMap.get(c);
+				if (ind != null) {
+					tMap.put(c, ++ind);
+					containAllChars = foundAll(tMap);
+				}
+				p1++;
+			}
 		}
-		int lo = -1, hi = s.length();
-		for (LinkedList<Integer> list : tMap.values()) {
-			if (list.isEmpty())
-				return "";
-
-			int first = list.getFirst(), last = list.getLast();
-
-			if (first > lo)
-				lo = first;
-			if (last < hi)
-				hi = last;
+		if (containAllChars) {
+			p2 = p2 > 0 ? --p2 : p2;
+			int length = p1 - p2;
+			if (length < minLength) {
+				ans[0] = p2;
+				ans[1] = p1;
+			}
 		}
 
-		return s.substring(lo, hi);
+		return s.substring(ans[0], ans[1]);
+	}
+
+	boolean foundAll(Map<Character, Integer> tMap) {
+		for (int count : tMap.values())
+			if (count == 0)
+				return false;
+		return true;
 	}
 
 	public static void main(String[] args) {
 		P76MinimumWindowSubstring p = new P76MinimumWindowSubstring();
-		Assert.assertEquals("BANC", p.minWindow("ADOBECODEBANC", "ABC"));
+		Assert.assertEquals("a", p.minWindow("ab", "a"));
+//		Assert.assertEquals("a", p.minWindow("a", "a"));
+//		Assert.assertEquals("BANC", p.minWindow("ADOBECODEBANC", "ABC"));
 	}
 
 }
