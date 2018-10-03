9b32e5d12ac12ad9fa783f2a5021b6bbeaabe177
Zheng Li <elighzn@gmail.com>
2018-10-03 00:42:48 -0500

    Add P401
    Change-Id: I73eeafe965f64c27f4dab1b150f9ca8611a112fe
    Signed-off-by: Zheng Li <elighzn@gmail.com>

--- src/main/java/lc/backtracking/easy/P401BinaryWatch.java
+++ src/main/java/lc/backtracking/easy/P401BinaryWatch.java
@@ -1,14 +1,13 @@
 package lc.backtracking.easy;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
 
 /**
- * A binary watch has 4 LEDs on the top which represent the hours (0-11), and
- * the 6 LEDs on the bottom represent the minutes (0-59).
+ * A binary watch has 4 LEDs on the top which represent the hours (0-11), and the 6 LEDs on the
+ * bottom represent the minutes (0-59).
  * 
- * Each LED represents a zero or one, with the least significant bit on the
- * right.
+ * Each LED represents a zero or one, with the least significant bit on the right.
  * 
  * 8 4 2 1
  * 
@@ -20,48 +19,65 @@ import java.util.List;
  * 
  * For example, the above binary watch reads "3:25".
  * 
- * Given a non-negative integer n which represents the number of LEDs that are
- * currently on, return all possible times the watch could represent.
+ * Given a non-negative integer n which represents the number of LEDs that are currently on, return
+ * all possible times the watch could represent.
  * 
  * Example:
  * 
  * Input: n = 1
  * 
- * Return: ["1:00", "2:00", "4:00", "8:00", "0:01", "0:02", "0:04", "0:08",
- * "0:16", "0:32"]
+ * Return: ["1:00", "2:00", "4:00", "8:00", "0:01", "0:02", "0:04", "0:08", "0:16", "0:32"]
  * 
  * Note:
  * 
  * The order of output does not matter.
  * 
- * The hour must not contain a leading zero, for example "01:00" is not valid,
- * it should be "1:00".
+ * The hour must not contain a leading zero, for example "01:00" is not valid, it should be "1:00".
  * 
- * The minute must be consist of two digits and may contain a leading zero, for
- * example "10:2" is not valid, it should be "10:02".
+ * The minute must be consist of two digits and may contain a leading zero, for example "10:2" is
+ * not valid, it should be "10:02".
  */
 public class P401BinaryWatch {
 
-	public List<String> readBinaryWatch(int num) {
-		List<String> ans = new ArrayList<>();
+  private static final int[] HOURS = {1, 2, 4, 8};
+  private static final int[] MINUTES = {1, 2, 4, 8, 16, 32};
 
-		readBinary(num, ans);
+  public List<String> readBinaryWatch(int num) {
+    List<String> ans = new Vector<>();
 
-		return ans;
-	}
+    readWatch(ans, num, 0, 0, 0, 0);
 
-	void readBinary(int num, List<String> ans) {
-		for (int i = 0; i < 4 && i < num; i++) {
-			
-		}
-	}
+    return ans;
+  }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+  void readWatch(List<String> ans, int num, int hr, int min, int iH, int iM) {
 
-		P401BinaryWatch p = new P401BinaryWatch();
-		System.out.println(p.readBinaryWatch(1));
+    if (hr > 11 || min > 59)
+      return;
 
-	}
+    if (num == 0) {
+      String a = hr + ":";
+      a += min < 10 ? "0" + min : min;
+      ans.add(a);
+    }
+
+    num--;
+
+    for (; iH < HOURS.length; iH++) {
+      readWatch(ans, num, hr + HOURS[iH], min, iH + 1, iM);
+    }
+
+    for (; iM < MINUTES.length; iM++) {
+      readWatch(ans, num, hr, min + MINUTES[iM], iH, iM + 1);
+    }
+  }
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+    P401BinaryWatch p = new P401BinaryWatch();
+    System.out.println(p.readBinaryWatch(1));
+    System.out.println(p.readBinaryWatch(2));
+  }
 
 }
