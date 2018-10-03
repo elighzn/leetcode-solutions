f4521d103010e7790c36687c1e32d6ecd0933954
Zheng Li <elighzn@gmail.com>
2018-10-02 23:41:01 -0500

    Add P784
    Change-Id: Ib96c4cf5bc232adb2e70e8a147595234ccda05af
    Signed-off-by: Zheng Li <elighzn@gmail.com>

---
+++ src/main/java/lc/backtracking/medium/P93RestoreIPAddresse.java
@@ -0,0 +1,29 @@
+package lc.backtracking.medium;
+
+import java.util.List;
+
+public class P93RestoreIPAddresse {
+
+	public List<String> restoreIpAddresses(String s) {
+		if (s == null || s.isEmpty())
+			return null;
+
+		char[] arr = s.toCharArray();
+
+	}
+
+	List<String> restoreIpAddresses(char[] arr, ) {
+		
+	}
+
+	boolean isIPAddress(String s) {
+		if (s.length() < 4 && s.length() > 12)
+			return false;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
