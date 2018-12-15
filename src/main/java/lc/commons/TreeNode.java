8e2ea4b5f1ef530f3f4bccb355da3df062e7716d
Zheng Li <zli@powercosts.com>
2018-12-14 18:19:08 -0600

    Change TreeNode Util
    Change-Id: Ia12d898d7f4f5e7da1fb19485066d8dcf2479e26
    Signed-off-by: Zheng Li <zli@powercosts.com>

--- src/main/java/lc/commons/TreeNode.java
+++ src/main/java/lc/commons/TreeNode.java
@@ -1,15 +1,31 @@
 package lc.commons;
 
 public class TreeNode {
-  public int val;
-  public TreeNode left;
-  public TreeNode right;
+	public int val;
+	public TreeNode left;
+	public TreeNode right;
 
-  TreeNode(int x) {
-    val = x;
-  }
+	public TreeNode(int x) {
+		val = x;
+	}
 
-  public String toString() {
-    return String.valueOf(val);
-  }
+	public TreeNode TreeNode(Integer[] nums) {
+		TreeNode root = null;
+		if (nums == null || nums.length < 1)
+			return null;
+
+		TreeNode node = root;
+
+		for (int i = 0; i < nums.length; i++) {
+			
+		}
+
+		return root;
+	}
+
+	
+
+	public String toString() {
+		return String.valueOf(val);
+	}
 }
